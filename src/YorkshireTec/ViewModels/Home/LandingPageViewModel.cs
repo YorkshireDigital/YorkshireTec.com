62fa766b66bdfaa155e9115afe49acfa2730030f
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-03-07 18:27:37 +0000

    removed commented out code

--- src/YorkshireTec/ViewModels/Home/LandingPageViewModel.cs
+++ src/YorkshireTec/ViewModels/Home/LandingPageViewModel.cs
@@ -11,11 +11,6 @@
         {
             Tweets = new List<string>();
 
-            //const string consumerKey = "cXTGDjf0MBNQWB1IvcaEnA";
-            //const string consumerSecret = "0Wz3oBFO4D5dn0LGtM7V6ZuIjLFl6Ac1r0Pt7mKA";
-            //const string accessToken = "183268831-C2mwzwFbOe7OPkebbtQKHCJpQM1CnmEW4Ibp4pw3";
-            //const string accessTokenSecret = "mqBOrRRVkxhN7HswBxD4yQdnmQqntQdTNiM1A3T3yP5Kt";
-
             var twitterClientInfo = new TwitterClientInfo { ConsumerKey = consumerKey, ConsumerSecret = consumerSecret };
             var twitterService = new TwitterService(twitterClientInfo);
 
