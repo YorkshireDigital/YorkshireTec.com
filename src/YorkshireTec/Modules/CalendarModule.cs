dfa40f366ca8816006f9c04b8e1a17b494a741e1
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-03-08 14:20:56 +0000

    list events

--- src/YorkshireTec/Modules/CalendarModule.cs
+++ src/YorkshireTec/Modules/CalendarModule.cs
@@ -1,13 +1,24 @@
 namespace YorkshireTec.Modules
 {
+    using System.Xml.Linq;
     using Nancy;
+    using YorkshireTec.ViewModels.Calendar;
 
     public class CalendarModule : NancyModule
     {
         public CalendarModule()
             : base("calendar")
         {
-            Get["/"] = _ => View["Index"];
+            const string calendarId = "info%40yorkshiretec.com";
+            Get["/"] = _ =>
+            {
+                var feed = XDocument.Load(string.Format("https://www.google.com/calendar/feeds/{0}/public/basic", calendarId));
+
+                var model = new CalendarIndexViewModel();
+                model.LoadFromAtomFeed(feed);
+
+                return Negotiate.WithModel(model).WithView("Index");
+            };
         }
     }
 }
