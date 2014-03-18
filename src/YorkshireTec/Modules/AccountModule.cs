1350cc38753c6345d51f9c2ed99f0b13d10479dc
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-03-18 21:30:32 +0000

    Authenticate user on logon

--- src/YorkshireTec/Modules/AccountModule.cs
+++ src/YorkshireTec/Modules/AccountModule.cs
@@ -1,13 +1,18 @@
 namespace YorkshireTec.Modules
 {
+    using System;
+    using System.Web.Helpers;
+    using global::Raven.Client;
     using Nancy;
+    using Nancy.Authentication.Forms;
     using Nancy.ModelBinding;
     using Nancy.Validation;
+    using YorkshireTec.Raven.Repositories;
     using YorkshireTec.ViewModels.Account;
 
     public class AccountModule : NancyModule
     {
-        public AccountModule()
+        public AccountModule(IDocumentSession documentSession)
             : base("account")
         {
             Get["/log-in"] = _ =>
@@ -20,11 +25,23 @@ namespace YorkshireTec.Modules
                 var model = this.Bind<AccountLogInViewModel>();
                 var result = this.Validate(model);
 
-                if (!result.IsValid)
+                if (result.IsValid)
                 {
-                    model.Username = "FAIL";
-                    return Negotiate.WithModel(model).WithView("LogIn");
+                    var userRepository = new UserRepository(documentSession);
+
+                    var user = userRepository.GetUser(model.Username);
+
+                    if (user != null)
+                    {
+                        if (Crypto.VerifyHashedPassword(user.Password, model.Password))
+                        {
+                            var expiry = model.RememberMe ? DateTime.Now.AddDays(7) : (DateTime?)null;
+                            return this.LoginAndRedirect(user.Id, expiry, "~/");
+                        }
+                    }
                 }
+
+                model.Username = "FAIL";
                 return Negotiate.WithModel(model).WithView("LogIn");
             };
         }
--- src/YorkshireTec/ViewModels/Account/AccountLogInViewModel.cs
+++ src/YorkshireTec/ViewModels/Account/AccountLogInViewModel.cs
@@ -6,6 +6,7 @@
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool RememberMe { get; set; }
     }
 
     public class AccountLogInViewModelValidator : AbstractValidator<AccountLogInViewModel>
