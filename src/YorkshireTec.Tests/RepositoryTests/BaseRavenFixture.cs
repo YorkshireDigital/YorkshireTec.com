ffda6c455152d90ba9de3a089f9afedc890e58b3
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-03-21 00:14:17 +0000

    removed guff added by RavenDB test helpers

--- src/YorkshireTec.Tests/RepositoryTests/BaseRavenFixture.cs
+++ src/YorkshireTec.Tests/RepositoryTests/BaseRavenFixture.cs
@@ -5,27 +5,23 @@
     using global::Raven.Tests.Helpers;
     using NUnit.Framework;
     using YorkshireTec.Raven;
-    using System.Transactions;
 
     [TestFixture]
     public class BaseRavenFixture : RavenTestBase
     {
         private DocumentStore documentStore;
         internal IDocumentSession DocumentSession;
-        private TransactionScope transaction;
 
         [SetUp]
         public void BaseSetUp()
         {
-            documentStore =  NewDocumentStore();
+            documentStore = NewDocumentStore();
             DocumentSession = documentStore.OpenSession();
-            
         }
 
         [TearDown]
         public void BaseTearDown()
         {
-            documentStore.Dispose();
             DocumentSession.Dispose();
         }
     }
