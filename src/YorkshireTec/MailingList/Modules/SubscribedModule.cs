4c428d8642108d966f0d71896a4e861bb54d5cd8
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-04-22 09:16:41 +0100

    added custom subscription confirmed page

---
+++ src/YorkshireTec/MailingList/Modules/SubscribedModule.cs
@@ -0,0 +1,22 @@
+﻿namespace YorkshireTec.MailingList.Modules
+{
+    using Nancy;
+    using YorkshireTec.Infrastructure;
+    using YorkshireTec.MailingList.ViewModels;
+
+    public class SubscribedModule : BaseModule
+    {
+        public SubscribedModule()
+            : base("MailingList/Subscribed")
+        {
+            Get["/"] = _ =>
+            {
+                var viewModel = new IndexViewModel();
+                var model = GetBaseModel(viewModel);
+
+                model.Page.Title = "Bringing Yorkshires digital community together";
+                return Negotiate.WithModel(model).WithView("Subscribed");
+            };
+        }
+    }
+}
