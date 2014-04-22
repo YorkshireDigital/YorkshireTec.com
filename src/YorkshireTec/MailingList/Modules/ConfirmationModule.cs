f28af3829d5b4402f7d2f508bed56532a5bbfefb
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-04-22 08:24:45 +0100

    Added custom mailing list confirmation page

---
+++ src/YorkshireTec/MailingList/Modules/ConfirmationModule.cs
@@ -0,0 +1,22 @@
+﻿namespace YorkshireTec.MailingList.Modules
+{
+    using Nancy;
+    using YorkshireTec.Infrastructure;
+    using YorkshireTec.MailingList.ViewModels;
+    
+    public class ConfirmationModule : BaseModule
+    {
+        public ConfirmationModule()
+            : base("MailingList/Confirmation")
+        {
+            Get["/"] = _ =>
+            {
+                var viewModel = new IndexViewModel();
+                var model = GetBaseModel(viewModel);
+
+                model.Page.Title = "Bringing Yorkshires digital community together";
+                return Negotiate.WithModel(model).WithView("Confirmation");
+            };
+        }
+    }
+}
