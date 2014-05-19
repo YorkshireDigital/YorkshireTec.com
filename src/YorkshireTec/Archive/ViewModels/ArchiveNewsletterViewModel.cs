a14ed0027d19fc78cb58068ef1be5082f2baa69c
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-05-19 21:31:47 +0100

    Added an archive page for the newsletter
    This pulls past campaigns directly from the MailChimp API

---
+++ src/YorkshireTec/Archive/ViewModels/ArchiveNewsletterViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace YorkshireTec.Archive.ViewModels
+{
+    using System.Collections.Generic;
+    using YorkshireTec.Infrastructure.Helpers;
+
+    public class ArchiveNewsletterViewModel
+    {
+        public List<MailChimpCampaign> Archives { get; set; }
+
+        public ArchiveNewsletterViewModel()
+        {
+            Archives = new List<MailChimpCampaign>();
+        }
+    }
+}
