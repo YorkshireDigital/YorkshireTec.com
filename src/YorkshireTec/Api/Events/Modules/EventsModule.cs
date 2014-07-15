52aa3d2ec582d4c0ab288c2617e694b3070dd507
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-07-15 20:41:13 +0100

    Removed all the old calendar guff

--- src/YorkshireTec/Api/Events/Modules/EventsModule.cs
+++ src/YorkshireTec/Api/Events/Modules/EventsModule.cs
@@ -1,31 +1,16 @@
-namespace YorkshireTec.Api.Calendar.Modules
+namespace YorkshireTec.Api.Events.Modules
 {
-    using System.Xml.Linq;
-    using Nancy;
     using NHibernate;
-    using YorkshireTec.Api.Calendar.ViewModels;
     using YorkshireTec.Api.Infrastructure;
 
-    public class CalendarModule : BaseModule
+    public class EventsModule : BaseModule
     {
-        public CalendarModule(ISessionFactory sessionFactory)
-            : base(sessionFactory, "calendar")
+        public EventsModule(ISessionFactory sessionFactory)
+            : base(sessionFactory, "events")
         {
             this.RequiresFeature("Calendar");
 
-            const string calendarId = "info%40yorkshiretec.com";
-            Get["/"] = _ =>
-            {
-
-                var feed = XDocument.Load(string.Format("https://www.google.com/calendar/feeds/{0}/public/basic", calendarId));
-
-                var viewModel = new CalendarIndexViewModel();
-                viewModel.LoadFromAtomFeed(feed);
-                var model = GetBaseModel(viewModel);
-                model.Page.Title = "Calendar";
-
-                return Negotiate.WithModel(model).WithView("Index");
-            };
+            Get["/"] = _ => 200;
         }
     }
 }
