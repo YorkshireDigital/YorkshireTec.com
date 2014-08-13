1e1e163700e1fe07f906e21abd65d11598119e92
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-08-13 23:17:58 +0100

    Added bare bones angularjs project

---
+++ src/YorkshireDigital.Web/Scripts/app/app.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    window.app = angular.module('yorkshireDigitalApp', [
+        'ngRoute'
+    ]);
+
+    app.config([
+        '$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            $httpProvider.defaults.useXDomain = true;
+
+            $routeProvider
+                .when('/', { templateUrl: '/Scripts/app/views/home/Home.html', controller: 'homeController' })
+                .when('/Error', { templateUrl: '/Scripts/app/views/shared/Error.html' })
+                .when('/404', { templateUrl: '/Scripts/app/views/shared/404.html' })
+                .otherwise({
+                    redirectTo: '/404'
+                });
+
+            $locationProvider.html5Mode(true);
+        }
+    ]);
+})();
---
+++ src/YorkshireDigital.Web/Scripts/app/controllers/homeController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    window.app
+        .controller('homeController', ['$scope', homeController]);
+
+    function homeController($scope) {
+        $scope.title = 'homeController';
+
+        init();
+
+        function init() { }
+    }
+})();
