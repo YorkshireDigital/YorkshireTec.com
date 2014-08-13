64e2142ba3888ceb8da39e2060c640f123bb1c77
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-08-13 19:01:03 +0100

    Added newsletter archives to the api project

--- src/YorkshireDigital.Api/Infrastructure/BaseModule.cs
+++ src/YorkshireDigital.Api/Infrastructure/BaseModule.cs
@@ -1,12 +1,22 @@
 ﻿namespace YorkshireDigital.Api.Infrastructure
 {
     using Nancy;
+    using NHibernate;
 
     public class BaseModule : NancyModule
     {
+        internal ISession RequestSession;
+
         public BaseModule()
         {
             Get["/Test"] = _ => "Success";
+        }
+
+
+        public BaseModule(ISessionFactory sessionFactory, string modulePath)
+            : base(string.Format("/{0}", modulePath))
+        {
+            RequestSession = sessionFactory.GetCurrentSession();
         }
     }
 }
