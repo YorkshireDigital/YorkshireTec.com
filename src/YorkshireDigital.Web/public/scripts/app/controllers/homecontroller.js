418f3af1eb1da345bd4deee61aa1360fa2d76978
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-10-13 16:11:05 +0100

    Added a custom filter to allow us to be a little more sophisticated with out filtering. And it works. BOOYA!

--- src/YorkshireDigital.Web/public/scripts/app/controllers/homecontroller.js
+++ src/YorkshireDigital.Web/public/scripts/app/controllers/homecontroller.js
@@ -10,19 +10,23 @@
         calendarService.Events.query(function (events) {
             $scope.events = events;
             var interests = [];
+            var locations = [];
             for (var i = events.length - 1; i >= 0; i--) {
                 for (var j = events[i].interests.length - 1; j >= 0; j--) {
                     if ($.inArray(events[i].interests[j], interests) === -1) {
                         interests.push(events[i].interests[j]);
                     }
                 }
+                if ($.inArray(events[i].location, locations) === -1) {
+                    locations.push(events[i].location);
+                }
             };
             $scope.interests = interests;
+            $scope.locations = locations;
+            $scope.search = {
+                interests: '',
+                location: ''
+            };
         });
-
-        init($scope);
-
-        function init($scope) { 
-        }
     }
 })();
