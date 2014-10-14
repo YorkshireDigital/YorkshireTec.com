82a500b53fed027b3e8259d209c227da61c3a96c
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-10-14 16:08:15 +0100

    Load the calendar events in three month blocks

--- src/YorkshireDigital.Web/public/scripts/app/controllers/homecontroller.js
+++ src/YorkshireDigital.Web/public/scripts/app/controllers/homecontroller.js
@@ -10,15 +10,48 @@
         $scope.to_trusted = function(html_code) {
             return $sce.trustAsHtml(html_code);
         };
+        $scope.clndrNextMonth = function() {
+            console.log('clndrNextMonth');
+            $scope.clndr.forward();
+            $scope.loadEvents(moment($scope.clndr.month._d), function (events) {
+                $scope.addNewEvents(events);
+            });
+        };
+        $scope.clndrPreviousMonth = function() {
+            console.log('clndrPreviousMonth');
+            $scope.clndr.back();
+            $scope.loadEvents(moment($scope.clndr.month._d), function (events) {
+                $scope.addNewEvents(events);
+            });
+        };
+        $scope.addNewEvents = function (events) {
+            var newEvents = [];
+            for (var e = events.length - 1; e >= 0; e--) {
+                var found = false;
+                for (var k = 0; k < $scope.events.length; k++) {
+                    if (events[e].uniqueName == $scope.events[k].uniqueName) {
+                        found = true;
+                    }
+                }
+                if (found === false) {
+                    newEvents.push(events[e]);
+                    $scope.events.push(events[e]);
+                }
+            };
+            $scope.populateFilters(newEvents);
+            $scope.clndr.addEvents(newEvents);
+            return newEvents;
+        };
+        $scope.loadEvents = function (month, callback) {
+            var from = month.date(1).subtract(1, 'M').format('DD/MM/YYYY');
+            var to = month.date(1).add(3, 'M').format('DD/MM/YYYY');
 
-        var to = moment().date(1).add(2, 'M').format('DD/MM/YYYY');
+            calendarService.Events.query({ from: from, to: to }, callback);
+        };
 
-        var from = moment().date(1).subtract(1, 'M').format('DD/MM/YYYY');
-
-        calendarService.Events.query({from: from, to: to}, function (events) {
-            $scope.events = events;
-            var interests = [];
-            var locations = [];
+        $scope.populateFilters = function (events) {
+            var interests = $scope.interests || [];
+            var locations = $scope.locations || [];
             for (var i = events.length - 1; i >= 0; i--) {
                 for (var j = events[i].interests.length - 1; j >= 0; j--) {
                     if ($.inArray(events[i].interests[j], interests) === -1) {
@@ -31,10 +64,15 @@
             };
             $scope.interests = interests;
             $scope.locations = locations;
-            $scope.search = {
+            $scope.search = $scope.search || {
                 interests: '',
                 location: ''
             };
+        };
+
+        $scope.loadEvents(moment(), function (events) {
+            $scope.events = events;
+            $scope.populateFilters(events);
         });
     }
 })();
--- src/YorkshireDigital.Web/public/scripts/app/filters/calendarFilter.js
+++ src/YorkshireDigital.Web/public/scripts/app/filters/calendarFilter.js
@@ -1,19 +1,15 @@
 ﻿window.app.filter('calendarFilter', function () {
     return function (events, filter) {
-        //console.log(filter);
         var result = [];
         for (var i = 0; i < events.length; i++) {
             var include = true;
             if (filter.location && events[i].location != filter.location) {
-                console.log(events[i].title + ' is not in ' + filter.location);
                 include = false;
             }
             if (filter.interests && events[i].interests.indexOf(filter.interests) == -1) {
-                console.log(events[i].title + ' does not have ' + filter.interests);
                 include = false;
             }
             if (filter.searchText && events[i].title.toLowerCase().indexOf(filter.searchText.toLowerCase()) == -1) {
-                console.log(events[i].title + ' does not match ' + filter.searchText);
                 include = false;
             }
             if (include) {
