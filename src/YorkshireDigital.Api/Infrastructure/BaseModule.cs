4e439931b59106a55cd70231c861e045fc48e42c
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-11-17 20:39:23 +0000

    Added sitemap and robot.txt

--- src/YorkshireDigital.Api/Infrastructure/BaseModule.cs
+++ src/YorkshireDigital.Api/Infrastructure/BaseModule.cs
@@ -1,21 +1,35 @@
 ﻿namespace YorkshireDigital.Api.Infrastructure
 {
+    using System;
     using Nancy;
     using Nancy.ModelBinding;
     using Nancy.Validation;
     using NHibernate;
     using YorkshireDigital.Api.Infrastructure.Models;
+    using YorkshireDigital.Api.Infrastructure.Responses;
+    using YorkshireDigital.Data.Services;
 
     public class BaseModule : NancyModule
     {
         internal ISession RequestSession;
 
-        public BaseModule()
+        public BaseModule(ISessionFactory sessionFactory)
         {
+            RequestSession = sessionFactory.GetCurrentSession();
+
+            var service = new EventService(RequestSession);
+
             Get["/"] = _ => HttpStatusCode.ImATeapot;
             Post["/"] = _ => HttpStatusCode.ImATeapot;
             Put["/"] = _ => HttpStatusCode.ImATeapot;
             Delete["/"] = _ => HttpStatusCode.ImATeapot;
+
+            Get["/sitemap"] = _ =>
+            {
+                var events = service.GetWithinRange(DateTime.MinValue, DateTime.MaxValue);
+
+                return new SitemapResponse(events, "http://www.yorkshiredigital.com/");
+            };
         }
 
 
---
+++ src/YorkshireDigital.Api/Infrastructure/Responses/SitemapResponse.cs
@@ -0,0 +1,49 @@
+﻿namespace YorkshireDigital.Api.Infrastructure.Responses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Linq;
+    using Nancy;
+    using YorkshireDigital.Data.Domain.Events;
+
+    public class SitemapResponse : Response
+    {
+        private readonly string siteUrl;
+
+        public SitemapResponse(IEnumerable<Event> model, string siteUrl)
+        {
+            this.siteUrl = siteUrl;
+            Contents = GetXmlContents(model);
+            ContentType = "application/xml";
+            StatusCode = HttpStatusCode.OK;
+        }
+
+        private Action<Stream> GetXmlContents(IEnumerable<Event> model)
+        {
+            var blank = XNamespace.Get(@"http://www.sitemaps.org/schemas/sitemap/0.9");
+
+            var xDocument = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), new XElement(blank + "urlset", new XAttribute("xmlns", blank.NamespaceName)));
+
+            foreach (var item in model)
+            {
+                xDocument.Root.Add(new XElement(blank + "url",
+                                    new XElement(blank + "loc", "http://www.yorkshiredigital.com/" + item.UniqueName),
+                    // TODO: Add LastModified to Event
+                                    new XElement(blank + "lastmod", DateTime.Now.ToString("yyyy-MM-dd")),
+                                    new XElement(blank + "changefreq", "weekly"),
+                                    new XElement(blank + "priority", "1.00")));
+            }
+
+            return stream =>
+            {
+                using (XmlWriter writer = XmlWriter.Create(stream))
+                {
+
+                    xDocument.Save(writer);
+                }
+            };
+        }
+    }
+}
