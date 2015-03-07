bb1600b8c31ef049926ad0bedf35c6614c5fe70d
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-03-07 13:03:05 +0000

    added mailinglist pages to the sitemap

--- src/YorkshireDigital.Web/Infrastructure/Responses/SitemapResponse.cs
+++ src/YorkshireDigital.Web/Infrastructure/Responses/SitemapResponse.cs
@@ -12,6 +12,14 @@
     {
         private readonly string siteUrl;
 
+        private readonly List<string> pages = new List<string>
+        {
+            "",
+            "mailinglist/confirmation",
+            "mailinglist/subscribed",
+            "mailinglist/archive",
+        };
+
         public SitemapResponse(IEnumerable<Event> model, string siteUrl)
         {
             this.siteUrl = siteUrl;
@@ -26,10 +34,20 @@
 
             var xDocument = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), new XElement(blank + "urlset", new XAttribute("xmlns", blank.NamespaceName)));
 
+            foreach (var page in pages)
+            {
+                xDocument.Root.Add(new XElement(blank + "url",
+                                    new XElement(blank + "loc", string.Format("{0}{1}", siteUrl, page)),
+                    // TODO: Add LastModified to Event
+                                    new XElement(blank + "lastmod", DateTime.Now.ToString("yyyy-MM-dd")),
+                                    new XElement(blank + "changefreq", "weekly"),
+                                    new XElement(blank + "priority", "1.00")));
+            }
+
             foreach (var item in model)
             {
                 xDocument.Root.Add(new XElement(blank + "url",
-                                    new XElement(blank + "loc", "http://www.yorkshiredigital.com/event/" + item.UniqueName),
+                                    new XElement(blank + "loc", string.Format("{0}event/{1}", siteUrl, item.UniqueName)),
                     // TODO: Add LastModified to Event
                                     new XElement(blank + "lastmod", DateTime.Now.ToString("yyyy-MM-dd")),
                                     new XElement(blank + "changefreq", "weekly"),
