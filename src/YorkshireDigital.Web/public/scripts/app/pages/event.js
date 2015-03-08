130b0d7874887dd6bbec733de9856ae1c22e16ed
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-03-08 01:39:31 +0000

    fixed a bug where the about and event details tabs didn't always switch correctly

--- src/YorkshireDigital.Web/public/scripts/app/pages/event.js
+++ src/YorkshireDigital.Web/public/scripts/app/pages/event.js
@@ -5,7 +5,7 @@
             $(this).parents('.tab-list__item').siblings().removeClass('tab-list__item--current');
             $(this).parents('.tab-list__item').addClass('tab-list__item--current');
 
-            var $tab = $($(this).attr('for'));
+            var $tab = $($(this).attr('for'), $(this).closest('.event__body'));
             $tab.siblings().removeClass('tab-pane--current');
             $tab.addClass('tab-pane--current');
         });
