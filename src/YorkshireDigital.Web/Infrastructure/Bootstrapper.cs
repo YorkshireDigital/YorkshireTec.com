3a9b0fd44fe8835a7810745b8df402d28fdbc2ca
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-03-08 04:05:10 +0000

    Enabled forms authentication

--- src/YorkshireDigital.Web/Infrastructure/Bootstrapper.cs
+++ src/YorkshireDigital.Web/Infrastructure/Bootstrapper.cs
@@ -4,8 +4,10 @@ namespace YorkshireDigital.Web.Infrastructure
     using System.Linq;
     using Humanizer;
     using Nancy;
+    using Nancy.Authentication.Forms;
     using Nancy.Bootstrapper;
     using Nancy.Conventions;
+    using Nancy.Cryptography;
     using Nancy.Diagnostics;
     using Nancy.TinyIoc;
     using NHibernate;
@@ -37,8 +39,7 @@ namespace YorkshireDigital.Web.Infrastructure
             Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat(context.ModulePath, "/Views/", viewName));
             Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat(context.ModulePath.Split('/').First(), "/Views/", viewName));
 
-            Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("public")
-        );
+            Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("public"));
         }
 
         protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
@@ -55,6 +56,30 @@ namespace YorkshireDigital.Web.Infrastructure
             context.ViewBag.Beta = new FeaturesModel().Beta;
 
             StaticConfiguration.DisableErrorTraces = false;
+
+            var cryptographyConfiguration =
+                new CryptographyConfiguration(
+                    new RijndaelEncryptionProvider(new PassphraseKeyGenerator("SuperSecretPass",
+                        new byte[]
+                        {
+                            100, 111, 110, 116, 32, 109, 97, 107, 101, 32, 109, 101, 32, 108, 111, 103, 32, 105, 110, 32,
+                            101, 97, 99, 104, 32, 116, 105, 109, 101, 32, 105, 32, 98, 117, 105, 108, 100, 33
+                        })),
+                    new DefaultHmacProvider(new PassphraseKeyGenerator("UberSecretPass",
+                        new byte[]
+                        {
+                            90, 71, 57, 117, 100, 67, 66, 116, 89, 87, 116, 108, 73, 71, 49, 108, 73, 71, 120, 118, 90, 121,
+                            66, 112, 98, 105, 66, 108, 89, 87, 78, 111, 73, 72, 82, 112, 98, 87, 85, 103, 97, 83, 66,
+                            105, 100, 87, 108, 115, 90, 67, 69, 61
+                        })));
+
+            var formsAuthConfiguration = new FormsAuthenticationConfiguration
+            {
+                CryptographyConfiguration = cryptographyConfiguration,
+                RedirectUrl = "~/account/log-in",
+                UserMapper = container.Resolve<IUserMapper>(),
+            };
+            FormsAuthentication.Enable(pipelines, formsAuthConfiguration);
         }
 
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
