9cfb5bcfc938b2d67af7f02b3e00524a7ea9a5d9
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-03-14 14:48:09 +0000

    Added a welcome page

---
+++ src/YorkshireDigital.Web/Account/Modules/AccountWelcomeModule.cs
@@ -0,0 +1,31 @@
+﻿namespace YorkshireDigital.Web.Account.Modules
+{
+    using System;
+    using Nancy;
+    using Nancy.Security;
+    using NHibernate;
+    using YorkshireDigital.Data.Services;
+    using YorkshireDigital.Web.Account.ViewModels;
+    using YorkshireDigital.Web.Infrastructure;
+
+    public class AccountWelcomeModule : BaseModule
+    {
+        public AccountWelcomeModule(ISessionFactory sessionFactory)
+            : base(sessionFactory, "Account/Welcome")
+        {
+            this.RequiresFeature("Account");
+            this.RequiresAuthentication();
+
+            Get["/"] = _ =>
+            {
+                var userSession = new UserService(RequestSession);
+                var user = userSession.GetUserById(new Guid(((UserIdentity)Context.CurrentUser).UserId));
+
+                var viewModel = new AccountWelcomeViewModel(user);
+                @ViewBag.Title = "Welcome";
+
+                return Negotiate.WithModel(viewModel).WithView("Welcome");
+            };
+        }
+    }
+}
---
+++ src/YorkshireDigital.Web/Account/ViewModels/AccountWelcomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace YorkshireDigital.Web.Account.ViewModels
+{
+    using System;
+    using YorkshireDigital.Data.Domain.Account;
+    using YorkshireDigital.Data.Domain.Account.Enums;
+
+    public class AccountWelcomeViewModel
+    {
+        public AccountWelcomeViewModel(User user)
+        {
+            UserId = user.Id;
+            Email = user.Email;
+            OnMailingList = user.MailingListState == MailingListState.Subscribed || user.MailingListState == MailingListState.PendingSubscribe;
+        }
+
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public bool OnMailingList { get; set; }
+    }
+}
