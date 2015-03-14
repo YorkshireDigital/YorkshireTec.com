11fde42f01617acf2b6ae68cb1ef78ab1bde5a9d
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-03-14 13:39:08 +0000

    Added a sign out at account/sign-out

---
+++ src/YorkshireDigital.Web/Account/Modules/AccountSignOutModule.cs
@@ -0,0 +1,16 @@
+﻿namespace YorkshireDigital.Web.Account.Modules
+{
+    using Nancy.Authentication.Forms;
+    using NHibernate;
+    using YorkshireDigital.Web.Infrastructure;
+
+    public class AccountSignOutModule : BaseModule
+    {
+        public AccountSignOutModule(ISessionFactory sessionFactory)
+            : base(sessionFactory, "account/sign-out")
+        {
+            this.RequiresFeature("Account");
+            Get["/"] = _ => this.LogoutAndRedirect("~/");
+        }
+    }
+}
