75ae6a3a060cecfb552e9a5fca9e6bbdb5c5153d
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-03-28 13:41:25 +0000

    Added a custom 404 page

---
+++ src/YorkshireDigital.Web/Infrastructure/Handlers/NotFoundErrorStatusCodeHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace YorkshireDigital.Web.Infrastructure.Handlers
+{
+    using Nancy;
+    using Nancy.ErrorHandling;
+    using Nancy.Responses.Negotiation;
+    using Nancy.ViewEngines;
+    using YorkshireDigital.Web.Infrastructure.Models;
+
+    public class NotFoundErrorStatusCodeHandler : DefaultViewRenderer, IStatusCodeHandler
+    {
+        private readonly IResponseNegotiator responseNegotiator;
+
+        public NotFoundErrorStatusCodeHandler(IViewFactory viewFactory, IResponseNegotiator responseNegotiator)
+            : base(viewFactory)
+        {
+            this.responseNegotiator = responseNegotiator;
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var response = RenderView(context, "PageNotFound", new PageNotFoundViewModel
+            {
+                Title = "Page Not Found!",
+                Summary = "We could not find the page you are looking for :("
+            });
+
+            response.StatusCode = statusCode;
+            context.Response = response;
+        }
+    }
+}
