6f6130b077a6a3029f15632fcedca78893722a98
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-04-01 22:11:00 +0100

    Added a helper class to decode and encode SSO urls from discourse

---
+++ src/YorkshireDigital.Data/Helpers/DiscourseHelper.cs
@@ -0,0 +1,61 @@
+﻿namespace YorkshireDigital.Data.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RestSharp.Contrib;
+
+    public interface IDiscourseHelper
+    {
+        bool ValidatePayloadSignature(string payload, string signature);
+        string GetHash(string payload);
+        object GetRedirectUrl(string discourseDomain, string name, string externalId, string email, string username, string nonce);
+    }
+
+    public class DiscourseHelper : IDiscourseHelper
+    {
+        private readonly string secret;
+
+        public DiscourseHelper(string secret)
+        {
+            this.secret = secret;
+        }
+
+        public bool ValidatePayloadSignature(string payload, string signature)
+        {
+            return signature == GetHash(payload);
+        }
+
+        public string GetHash(string payload)
+        {
+            var encoding = new UTF8Encoding();
+            var keyBytes = encoding.GetBytes(secret);
+
+            var hasher = new System.Security.Cryptography.HMACSHA256(keyBytes);
+
+            var bytes = encoding.GetBytes(payload);
+            var hash = hasher.ComputeHash(bytes);
+
+            return hash.Aggregate(string.Empty, (current, x) => current + String.Format("{0:x2}", x));
+        }
+
+        public object GetRedirectUrl(string discourseDomain, string name, string externalId, string email, string username, string nonce)
+        {
+            var payload = string.Format("nonce={0}&name={1}&username={2}&email={3}&external_id={4}", HttpUtility.UrlEncode(nonce), HttpUtility.UrlEncode(name), HttpUtility.UrlEncode(username), HttpUtility.UrlEncode(email), HttpUtility.UrlEncode(externalId));
+            var payload64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payload));
+            var payloadChunked = String.Join("\n", ChunksUpto(payload64, 60)) + "\n";
+            var payloadUrl = HttpUtility.UrlEncode(payloadChunked);
+
+            var hash = GetHash(payloadChunked);
+
+            return string.Format("{0}/session/sso_login?sso={1}&sig={2}", discourseDomain, payloadUrl, hash);
+        }
+
+        private static IEnumerable<string> ChunksUpto(string str, int maxChunkSize)
+        {
+            for (var i = 0; i < str.Length; i += maxChunkSize)
+                yield return str.Substring(i, Math.Min(maxChunkSize, str.Length - i));
+        }
+    }
+}
