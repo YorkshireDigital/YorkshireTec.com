4a2a91e45e0209a37717acc1fec556469f98f118
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-04-03 15:58:11 +0100

    Moved the event service interface into the same file to keep the project cleaner

--- src/YorkshireDigital.Data/Services/EventService.cs
+++ src/YorkshireDigital.Data/Services/EventService.cs
@@ -7,6 +7,15 @@
     using global::NHibernate.Linq;
     using YorkshireDigital.Data.Domain.Events;
 
+    public interface IEventService
+    {
+        void Save(Event myEvent);
+        Event Get(string uniqueName);
+        void Delete(Event eventToDelete);
+        List<Event> GetWithinRange(DateTime from, DateTime to);
+        List<Event> Query(DateTime? from, DateTime? to, string[] interests, string[] locations, int? skip, int? take);
+    }
+
     public class EventService : IEventService
     {
         private readonly ISession session;
