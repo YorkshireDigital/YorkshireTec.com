2074ce01b9adbca0bfa57f3c1b13a98438826560
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-04-24 02:15:57 +0100

    Fixed a bug with linking twitter accounts when there is no access token expiry

--- src/YorkshireDigital.Data/Domain/Account/Provider.cs
+++ src/YorkshireDigital.Data/Domain/Account/Provider.cs
@@ -9,24 +9,28 @@
         public virtual string Name { get; set; }
         public virtual string PublicToken { get; set; }
         public virtual string SecretToken { get; set; }
-        public virtual DateTime ExpiresOn { get; set; }
+        public virtual DateTime? ExpiresOn { get; set; }
         public virtual string Username { get; set; }
 
         public virtual bool Expired
         {
-            get { return ExpiresOn != new DateTime() && ExpiresOn < DateTime.Now; }
+            get { return ExpiresOn.HasValue && ExpiresOn.Value != new DateTime() && ExpiresOn.Value < DateTime.Now; }
         }
 
         public static Provider FromAuthenticatedClient(IAuthenticatedClient authenticatedClient)
         {
-            return new Provider
+            var provider = new Provider
             {
                 Name = authenticatedClient.ProviderName,
                 PublicToken = authenticatedClient.AccessToken.PublicToken,
                 SecretToken = authenticatedClient.AccessToken.SecretToken,
-                ExpiresOn = authenticatedClient.AccessToken.ExpiresOn,
                 Username = authenticatedClient.UserInformation.UserName
             };
+            if (authenticatedClient.AccessToken.ExpiresOn != new DateTime())
+            {
+                provider.ExpiresOn = authenticatedClient.AccessToken.ExpiresOn;
+            }
+            return provider;
         }
     }
 }
