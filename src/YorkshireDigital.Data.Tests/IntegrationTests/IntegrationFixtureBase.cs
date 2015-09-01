31518632096c4c6d83d3a4c5888b685e6ceb0119
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-09-01 23:18:16 +0100

    Added integration test setup and NHibernate mapping tests

---
+++ src/YorkshireDigital.Data.Tests/IntegrationTests/IntegrationFixtureBase.cs
@@ -0,0 +1,26 @@
+﻿using NHibernate;
+using NUnit.Framework;
+
+namespace YorkshireDigital.Data.Tests.IntegrationTests
+{
+    public class IntegrationFixtureBase
+    {
+        ITransaction _transaction;
+        [SetUp]
+        public void BaseSetup()
+        {
+            Session = IntegrationSessionFactoryProvidor.Instance.OpenSession();
+            _transaction = Session.BeginTransaction();
+        }
+
+        [TearDown]
+        public void BaseTearDown()
+        {
+            _transaction.Rollback();
+            if (Session != null)
+                Session.Dispose();
+        }
+
+        protected ISession Session { get; private set; }
+    }
+}
