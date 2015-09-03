e56ad5946f4109c55241263916a928a220e080fa
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-09-03 18:57:27 +0100

    #54 Added windows service to handle hangfire processing

---
+++ src/YorkshireDigital.Hangfire/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Hangfire.Logging;
+using Hangfire.Logging.LogProviders;
+using Microsoft.Owin.Hosting;
+using Topshelf;
+using Serilog;
+
+namespace YorkshireDigital.Hangfire
+{
+    class Program
+    {
+        private const string Endpoint = "http://localhost:12346";
+
+        static void Main()
+        {
+            LogProvider.SetCurrentLogProvider(new ColouredConsoleLogProvider());
+
+            HostFactory.Run(x =>
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.RollingFile(AppDomain.CurrentDomain.BaseDirectory + "\\logs\\app-{Date}.log")
+                    .WriteTo.ColoredConsole()
+                    .MinimumLevel.Information()
+                    .CreateLogger();
+
+                x.Service<Application>(s =>
+                {
+                    s.ConstructUsing(name => new Application());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.UseSerilog();
+
+                x.SetDescription("YorkshireDigital Hangfire Service");
+                x.SetDisplayName("YorkshireDigital Hangfire Service");
+                x.SetServiceName("yorkshiredigital-hangfire");
+            });
+        }
+
+        private class Application
+        {
+            private IDisposable _host;
+
+            public void Start()
+            {
+                _host = WebApp.Start<Startup>(Endpoint);
+
+                Console.WriteLine();
+                Console.WriteLine("Hangfire Server started.");
+                Console.WriteLine("Dashboard is available at {0}/hangfire", Endpoint);
+                Console.WriteLine();
+            }
+
+            public void Stop()
+            {
+                _host.Dispose();
+            }
+        }
+    }
+}
---
+++ src/YorkshireDigital.Hangfire/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(YorkshireDigital.Hangfire.Startup))]
+
+namespace YorkshireDigital.Hangfire
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseWelcomePage("/");
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Database", new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) });
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            SetUpSystemJobs();
+        }
+
+        public void SetUpSystemJobs()
+        {
+            // Set up recurring system tasks here
+        }
+    }
+}
